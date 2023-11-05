8cf56ba57b95d5108992955c46575c6a9c8ea46f
Rostyslav Liashenko <rostyslav.liashenko@unibrix.com>
2023-11-05 13:44:56 +0200

    feat: add skeleton bot

---
+++ src/main/java/com/liashenko/applicant/bot/ApplicantBot.java
@@ -0,0 +1,45 @@
+package com.liashenko.applicant.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class ApplicantBot extends TelegramLongPollingBot {
+    @Value("${bot.userName}")
+    private String userName;
+
+    public ApplicantBot(@Value("${bot.token}") String token) {
+        super(token);
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (!update.hasMessage()) return;
+
+        String messageText = update.getMessage().getText();
+        Long chatId = update.getMessage().getChatId();
+        System.out.println(messageText);
+        System.out.println(chatId);
+        this.sendMessage(chatId, "test");
+    }
+
+    @Override
+    public String getBotUsername() {
+        return this.userName;
+    }
+
+    private void sendMessage(Long chatId, String text) {
+        String chatIdStr = chatId.toString();
+        SendMessage sendMessage = new SendMessage(chatIdStr, text);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException telegramApiException) {
+            System.out.println("Error: " + telegramApiException.getMessage());
+        }
+    }
+}
